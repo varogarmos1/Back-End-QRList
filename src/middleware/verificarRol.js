c0a4d8e87a357704b3747aca626508d39949b33d
varogarmos1 <alvaro.garcia.mosquera@alumnos.upm.es>
2025-03-12 13:45:25 +0100

    Mejora middleware

--- src/middleware/verificarRol.js
+++ src/middleware/verificarRol.js
@@ -1,11 +1,18 @@
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 
+dotenv.config();
+
 export function verificarRol(...rolesPermitidos) {
     return (req, res, next) => {
-        if (!req.usuario || !rolesPermitidos.includes(req.usuario.rol)) {
+        if (!req.usuario) {
+            return res.status(401).json({ error: 'Acceso denegado. Usuario no autenticado' });
+        }
+
+        if (!rolesPermitidos.includes(req.usuario.rol)) {
             return res.status(403).json({ error: 'Acceso denegado. Permisos insuficientes' });
         }
+
         next();
     };
 }
